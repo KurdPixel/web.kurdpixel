57ac731c9b0ba8e688d016bed8ae7e916dcda262
ayadshkur10221 <ayadshkur.offical@gmail.com>
2026-01-06 17:29:44 +0300

    font

--- src/app/staff/page.tsx
+++ src/app/staff/page.tsx
@@ -3,17 +3,17 @@ import React from "react";
 export default function StaffPage() {
   return (
     <section className="pt-28">
-      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
+      <div className="py-8 px-4 mx-auto text-center lg:py-16 lg:px-6">
         <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
         <div className="mx-auto mb-8 max-w-screen-sm lg:mb-16">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">ستافی <span className="text-violet-500">کوردپیکسڵ</span></h2>
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white">ستافی <span className="text-violet-500">کوردپیکسڵ</span></h2>
           <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">سایتەکەمان بە هاوکاری تیمێکی پسپۆڕ و دڵسۆز کاردەکات. هەر یەکێک لە ئەندامانی تیمەکەمان بەشدارن لە دروستکردنی باشترین ئەزموونی سینەمایی بۆ ئێوە.</p>
         </div>
 
         <div className="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           <div className="text-center text-gray-500 dark:text-gray-400">
             <img className="mx-auto mb-4 w-36 h-36 rounded-full" src="https://media.discordapp.net/attachments/1456019679486414891/1458066288629911592/Screenshot_2025-12-16_112551.png?ex=695e498c&is=695cf80c&hm=d66440ddf9bf64653983c24d6a21bc17f16431611fb30f643d3d06d9bd972042&=&format=webp&quality=lossless" alt="Bonnie Avatar" />
-            <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <h3 className="mb-1 text-2xl font-bold tracking-tight text-white">
               <a href="#">Haryad</a>
             </h3>
             <p>Co-niggers</p>
@@ -24,10 +24,6 @@ export default function StaffPage() {
             <img className="mx-auto mb-4 w-36 h-36 rounded-full" src="https://media.discordapp.net/attachments/1112629062893056113/1455265869637746718/Screenshot_2025-12-29_212624.png?ex=695dfcb4&is=695cab34&hm=cf156a91bf34c5845011a079132d6931304aae1b2c6e8224fc04e0bff9c2421b&=&format=webp&quality=lossless" alt="Helene Avatar" />
             <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">Aland</a></h3>
             <p>Co-milker</p>
-            <ul className="flex justify-center mt-4 space-x-4">
-              <li><a href="#" className="text-[#39569c] hover:text-gray-900 dark:hover:text-white">{/* icon */}</a></li>
-              <li><a href="#" className="text-[#00acee] hover:text-gray-900 dark:hover:text-white">{/* icon */}</a></li>
-            </ul>
           </div>
 
           <div className="text-center text-gray-500 dark:text-gray-400">
